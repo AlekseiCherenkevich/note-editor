491b8c6fe29ef26eb7a060b39fd7732b490e73b4
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-03-25 10:11:55 +0300

    fix: fix local storage data

--- src/hooks/use-app.ts
+++ src/hooks/use-app.ts
@@ -4,20 +4,26 @@ import {v1} from "uuid";
 
 export type NoteType = {id:string, text: string, tags: string[]}
 
+
+const getLocalStorageData = () => {
+  const data = localStorage.getItem('notes')
+  return data ? JSON.parse(data) : {notes: [], filters: []}
+}
+
 export const useApp = () => {
   const [text, setText] = useState('')
   const [tags, setTags] = useState<string[]>([])
-  const [notes, setNotes]  = useState<NoteType[]>([])
-  const [filters, setFilters] = useState<string[]>([])
+  const [notes, setNotes]  = useState<NoteType[]>(getLocalStorageData().notes)
+  const [filters, setFilters] = useState<string[]>(getLocalStorageData().filters)
   const [activeFilters, setActiveFilters] = useState<string[]>([])
 
   const changeNote = changeNoteUtil(setText, setTags)
 
   const addNote = () => {
-    setNotes(prevState => [...prevState, {id: v1(), text: text, tags: tags}])
+    const newNote = {id: v1(), text: text, tags: tags}
+    setNotes(prevState => [...prevState, newNote])
     setText('')
     setTags([])
-
   }
 
   const changeActiveFilter = (filter: string) => {
@@ -37,6 +43,7 @@ export const useApp = () => {
       })
     })
     setFilters(allTags)
+    localStorage.setItem('notes', JSON.stringify({notes, filters}))
   },[notes])
 
   const filteredNotes = notes.filter(note=>{
